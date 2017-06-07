52f3d7702b347b402906cd6d7a06a9d10831099c
Radoslav Naydenov <em9961@gmail.com>
2017-06-07 22:58:40 +0300

    Modify api to get event link and send back total price of ticket

--- backend/controllers/apiController.js
+++ backend/controllers/apiController.js
@@ -1,7 +1,10 @@
+'use strict';
+
 const User = require('../models/user');
 const bodyParser = require('body-parser');
 const airbnb = require('airapi');
-
+const request = require ('request');
+const cheerio = require('cheerio');
 
 
 module.exports = (app) => {
@@ -12,20 +15,12 @@ module.exports = (app) => {
   app.use(bodyParser.urlencoded({extended: true}));
 
 
-  app.get("/api/users",(req, res) => {
-
-    User.find({}, (err, users) => {
-      if (err) throw err;
-      res.send(users);
-    });
-  });
-
-  app.get("/api/airbnb",(req,res) => {
+  app.get("/api/housing",(req,res) => {
     airbnb.search({
-     location: 'Sofia, Bulgaria',
-     checkin: '07/03/2015',
-     checkout: '07/06/2015',
-     guests: 2,
+     location: req.query.location,
+     checkin: req.query.checkIn,
+     checkout: req.query.checkOut,
+     guests: req.query.people || 1,
      page: 2,
      ib: true
     }).then(function(searchResults) {
@@ -33,4 +28,30 @@ module.exports = (app) => {
     });
   })
 
+
+  app.get("/api/festival",(req,res) => {
+    const url = "https://www.residentadvisor.net/event.aspx?934368";
+    request(url, (err,resp,body) => {
+      const $ = cheerio.load(body);
+      const event = $('.onsale');
+      const eventSale = event.children().children().children().text();
+      if (eventSale) {
+        const priceBreakdown = eventSale.split(/[£*$*€*\+*]/);
+        let ticketPrice = priceBreakdown[1];
+        let bookingFee = priceBreakdown[3];
+
+        // Euro symbol is placed after price in the RA website
+        if (eventSale.includes('€')) {
+          ticketPrice = priceBreakdown[0];
+          bookingFee = priceBreakdown[2];
+        }
+
+        // Check if booking fee is specified
+        bookingFee ? console.log(eval(`${ticketPrice} + ${bookingFee}`)) : console.log(ticketPrice)
+      } else {
+        console.log('Event has probably sold out.');
+      }
+    })
+  })
+  
 }
