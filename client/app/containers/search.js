bce62fc918f309843623a8df54050c49fd1f1da0
Radoslav Naydenov <em9961@gmail.com>
2017-06-12 16:27:25 +0300

    Making request to server using bluebird

--- client/app/containers/search.js
+++ client/app/containers/search.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import API from 'qpx-express';
 import { searchFestival, handleInput } from '../actions/searchActions';
-import airports from 'airport-codes';
-import airbnb  from 'airapi';
+import { coroutine as co} from 'bluebird';
 
 
 const apiKey = 'AIzaSyBtSws0VCIlzHDhXOpn9KR_3Juw9pIxe1Q';
@@ -13,6 +11,31 @@ const apiKey2 = 'AIzaSyD8yemJXcjTgYV7d_goyxKuwKNpd35VhXw';
 
 
 class Search extends React.Component {
+
+
+
+  componentDidMount() {
+    co(function * (){
+      let eventDetails = yield fetch('http://localhost:3000/api/prices/events?eventName=Afterlife Barcelona');
+      const eventDetails_json = yield eventDetails.json();
+      let destination = `${eventDetails_json.city},${eventDetails_json.country}`;
+      console.log(eventDetails_json);
+      const origin = 'Sofia,Bulgaria';
+      let date = eventDetails_json.date;
+      console.log(date);
+      let flightDetails = yield fetch(`http://localhost:3000/api/prices/flights?origin=${origin}&destination=${destination}&date=${date}`);
+      const flightDetails_json = yield flightDetails.json();
+      let housingDetails = yield fetch(`http://localhost:3000/api/prices/housing?location=${destination}&checkInDate=${date}`);
+      const housingDetails_json = yield housingDetails.json();
+      const obj = {
+        eventDetails_json,
+        flightDetails_json,
+        housingDetails_json
+      }
+
+      console.log(obj);
+    }).bind(this)();
+  }
 
   render () {
 
@@ -28,80 +51,6 @@ class Search extends React.Component {
     // })
 
 
-
-
-      // ---- Getting iata code ----- //
-
-    function getCityCode (cityName, countryName) {
-      const londonAirports = airports.where({city: cityName, country: countryName});
-
-      if (londonAirports.length > 1) {
-        for (let airport of londonAirports) {
-            const {attributes : result} = airport;
-            if (result.name == 'All Airports') {
-              return result.iata;
-            }
-        }
-      }
-      else {
-        // console.log(londonAirports[0].attributes);
-        return londonAirports[0].attributes.iata;
-      }
-    }
-
-    const sofiaIata = getCityCode('Sofia','Bulgaria');
-    const parisIata = getCityCode('Paris','France');
-    // console.log(barcelonaIata);
-
-    // ---- Getting data from flights ----- //
-
-    // const qpx = new API(apiKey2);
-    //
-    // const body = {
-    //     "request": {
-    //         "passengers": { "adultCount": 1 },
-    //         "slice": [{
-    //             "origin": sofiaIata,
-    //             "destination": parisIata,
-    //             "date": '2017-12-05'
-    //           }
-    //         ]
-    //       }
-    //     };
-    //
-    //
-    // qpx.getInfo(body, (err,data) => {
-    //   const {trips : { tripOption : tripsArr } } = data;
-    //   for (let trip of tripsArr) {
-    //     let stops = trip.slice[0].segment;
-    //     let pricing = trip.pricing[0];
-    //     if (stops.length == 1) {
-    //       let {saleTotal : flightPrice} = pricing;
-    //       let flight = stops[0].flight;
-    //
-    //       console.log(`Flight ${flight.carrier} ${flight.number} costs ${flightPrice}`);
-    //     }
-    //   }
-    // })
-
-
-    const airbnbSearch = fetch('/api/airbnb');
-
-
-    const airBnbImg = () => {
-
-      let imgSrc;
-
-      airbnbSearch.then(res => {
-        res.json().then(data => {
-          const {results_json : {search_results : properties } } = data;
-          imgSrc = "'" + properties[0].listing.picture_url + "'" ;
-          console.log(imgSrc);
-        })
-      })
-      
-
-      }
 
 
 
@@ -120,7 +69,7 @@ class Search extends React.Component {
       <div>
         <input placeholder = 'Search for a festival' onBlur = {(e) => this.props.handleInput(e)}></input>
         <button onClick = {() => this.props.searchFestival({festivalName})}> Go </button>
-        {airBnbImg()}
+
         {selectedFestival}
 
       </div>
