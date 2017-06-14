f63ab7a84ed8c758461cc8a5e6255218db34ecb2
Radoslav Naydenov <em9961@gmail.com>
2017-06-14 11:42:23 +0300

    Add seed festival data

--- backend/controllers/setupController.js
+++ backend/controllers/setupController.js
@@ -1,55 +1,64 @@
-const User = require ('../models/user');
+const Festival = require ('../models/festival');
 
 module.exports = (app) => {
 
 
-  app.get("/api/setupUsers",(req, res) => {
+  app.get("/api/setupFestivals",(req, res) => {
 
 
-    const staterUsers = [
+    const festivals = [
       {
-        username : 'kick420',
-        firstName : 'Deyan',
-        lastName : 'Velev',
-        likedFestivals : [{
-          festivalId : 1,
-          festivalName : 'Awakenings'
-        },
-        {
-          festivalId : 2,
-          festivalName : 'Solar Summer'
-        }]
+        name: "Glastonbury Festival 2017"
       },
       {
-        username : 'rnaidenov',
-        firstName : 'Radoslav',
-        lastName : 'Naydenov',
-        likedFestivals : [{
-          festivalId : 1,
-          festivalName : 'Awakenings'
-        },
-        {
-          festivalId : 3,
-          festivalName : 'Junction 2'
-        }]
+        name: "Awakenings Festival 2017 - Day 1"
       },
       {
-        username : 'kamisese',
-        firstName : 'Kamelia',
-        lastName : 'Mateeva',
-        likedFestivals : [{
-          festivalId : 4,
-          festivalName : 'Time Warp'
-        },
-        {
-          festivalId : 2,
-          festivalName : 'Solar Summer'
-        }]
+        name: "Awakenings Festival 2017 - Day 2"
+      },
+      {
+        name: "Noname 2017 • Music & Art Festival"
+      },
+      {
+        name: "EXIT Festival 2017"
+      },
+      {
+        name: "Kappa FuturFestival 2017"
+      },
+      {
+        name: "Full Moon Fest 2017"
+      },
+      {
+        name: "Crossroads 2017"
+      },
+      {
+        name: "Farr Festival 2017"
+      },
+      {
+        name: "DGTL Barcelona 2017"
+      },
+      {
+        name: "Sunfall Festival 2017"
+      },
+      {
+        name: "Lost Village 2017"
+      },
+      {
+        name: "South West Four 2017 - Day One"
+      },
+      {
+        name: "South West Four 2017 - Day Two"
+      },
+      {
+        name: "Sub Club Soundsystem 2017"
+      },
+      {
+        name: "Mysteryland 2017"
       }
     ]
 
 
-    User.create(staterUsers, (err, data) => {
+    Festival.create(festivals, (err, data) => {
       res.send(data);
     });
 
