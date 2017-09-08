1c9f31da86f19ba68a09503e4c7e460d6cf80988
46323Ra <radoslav.naydenov@yell.com>
2017-09-08 17:20:01 +0100

    Remove console logs

--- backend/app.js
+++ backend/app.js
@@ -17,11 +17,9 @@ setupController(app);
 apiController(app);
 
 app.use(express.static(path.join(__dirname + '/', '..', '/client/dist/')));
-console.log("PLEEEASE : " + path.join(__dirname + '/', '..', '/client/dist/'));
 
 app.get('/',(req,res) => {
     res.sendFile(path.join(__dirname,'../client/dist','index.html'));
 });
-
 
 app.listen(port);
