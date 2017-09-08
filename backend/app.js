82a2bd3a6a1cecd443b2c0252b53514fda4ad1c6
46323Ra <radoslav.naydenov@yell.com>
2017-09-08 16:48:08 +0100

    Change static path for express

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ setupController(app);
 apiController(app);
 
 app.use(express.static(path.join(__dirname + '/', '..', '/client/dist/')));
-console.log("PLEASE : " + path.join(__dirname + '/', '..', '/client/dist/'));
+console.log("PLEEEASE : " + path.join(__dirname + '/', '..', '/client/dist/'));
 
 app.get('/',(req,res) => {
     res.sendFile(path.join(__dirname,'../client/dist','index.html'));
