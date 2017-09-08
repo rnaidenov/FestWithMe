da91975790e691b65a083ccfcfa191476e07d7a4
46323Ra <radoslav.naydenov@yell.com>
2017-09-08 17:59:50 +0100

    Change name of mlab global variable

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ app.use(cors());
 
 var port = process.env.PORT || 3000;
 
-mongoose.connect(process.env.MONGOLAB_URI);
+mongoose.connect(process.env.MONGODB_URI);
 setupController(app);
 apiController(app);
 
