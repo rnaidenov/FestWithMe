faed976e9d1d3a72dbca9fa3b774cea7475eaec5
46323Ra <radoslav.naydenov@yell.com>
2018-03-18 00:36:22 +0000

    Add service-worker registration script

---
+++ client/dist/service-worker-registration.js
@@ -0,0 +1,9 @@
+(() => {
+    if('serviceWorker' in navigator){
+        window.addEventListener('load', () => {
+            navigator.serviceWorker.register('/service-worker.js')
+                                        .then(() => console.log("Successfully registered service worker!"))
+                                        .catch(err => console.log("Something went wrong with the SW registration. Error: ", err));
+        }
+    )}
+})();  
