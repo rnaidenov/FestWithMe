7993c4108bb0f97e37856d63ddec873bbbac34e7
Radoslav Naydenov <em9961@gmail.com>
2018-04-26 00:37:02 +0100

    Revert back to the original version for retriving location iata codes

--- backend/helpers/flights.js
+++ backend/helpers/flights.js
@@ -1,8 +1,14 @@
 const fetch = require('node-fetch');
 const config = require('../config');
 const Formatter = require('./formatter');
-const amadeusAPI = `https://api.sandbox.amadeus.com/v1.2/airports/autocomplete?apikey=${config.amadeusAPIKey}`;
+const amadeusAPI = `https://api.sandbox.amadeus.com/v1.2//airports/nearest-relevant?apikey=${config.amadeusAPIKey}`;
+const googleMaps = require('@google/maps').createClient({
+  key: config.googleMapsKey
+});
 const SKYSCANNER_BASE_URL =  `https://www.skyscanner.net/transport/flights`
+
+
+
 
 // Returns a 3-letter code of city, where nearest airport is located
 // const getCityCode = location => {
@@ -18,16 +24,34 @@ const SKYSCANNER_BASE_URL =  `https://www.skyscanner.net/transport/flights`
 //   });
 // }
 
-const _getCityIATACode = location => {
-  return new Promise(async (resolve,reject) => {
-    try {
-      const locationAirportsCityCodes = await fetch(`${amadeusAPI}&term=${location}`).then(res => res.json());
-      locationAirportsCityCodes.length ? resolve(locationAirportsCityCodes[0].value) : reject(`Unable to fetch IATA code for ${location}`);
-    } catch(err){
-      reject(`Unable to fetch IATA code for ${location}`)
-    }
-  })
+
+
+// Gets latitude and longitude of location
+const _getCoordinates = location => {
+  return new Promise((resolve, reject) => {
+    googleMaps.geocode({ address: location }, (err, response) => {
+      const { geometry: { location: { lat, lng } } } = response.json.results[0];
+      resolve({ lat, lng });
+    });
+  });
 }
+
+// Returns a 3-letter code of city, where nearest airport is located
+const _getCityIATACode = location => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const  { lat, lng } = await _getCoordinates(location);
+      const data = await fetch(`${amadeusAPI}&latitude=${lat}&longitude=${lng}`).then(res => res.json());
+      const { city } = data[0];
+      resolve(city);
+    } catch(err) {
+      reject(`Unable to fetch IATA code for ${location}`);
+    }
+  });
+}
+
+
+
 
 // https://www.skyscanner.net/transport/flights/lon/sof/180430?adults=1&currency=EUR
 
@@ -67,7 +91,7 @@ const generateSearchQuery = (origin, destination, date) => {
   })
 }
 
-generateSearchQuery('London','Glasgow','28 June 2018').then(res => console.log(res));
+generateSearchQuery('London, United Kingdom','Mannheim','28 June 2018').then(res => console.log(res));
 
 
 const _getFlightPriceDetails = async (searchQuery, results, currency) => {
