be4260e81ae724f1d7dd44d6d99c35d6c2213b2e
46323Ra <radoslav.naydenov@yell.com>
2018-05-06 16:36:36 +0100

    Update currency converter tests

--- backend/test/currencyConverter.test.js
+++ backend/test/currencyConverter.test.js
@@ -5,29 +5,31 @@ const CurrencyConverter = require('../helpers/currencies');
 
 
 describe('Currency converter', () => {
-    _convertPrice('$','£',328,'GBP');
-    _convertPrice('£','£',420,'GBP');
-    _convertPrice('£','€',794,'EUR');
+    _convertPrice('$', '£', 328, '£');
+    _convertPrice('£', '£', 420, '£');
+    _convertPrice('£', '€', 794, '€');
 });
 
 
-
-function _convertPrice (from, to, amount, expectedCurrencyCode) {
-    it(`Convert ${from}${amount} to ${to}`, function() {
+function _convertPrice(from, to, amount, expectedCurrencySymbol) {
+    it(`Convert ${from}${amount} to ${to}`, function () {
         this.timeout(config.testsMaxTimeout);
 
         return CurrencyConverter.convert(from, to, amount)
-                .then(convertedData => {
-                    expect(convertedData).to.have.all.keys('convertedAmount','currency');
-                    const { convertedAmount, currency } = convertedData;
-                    expect(convertedAmount).to.be.a('number','Converted amount should have been a number, but it is a ',typeof(convertedAmount),'.');
-                    assert(expectedCurrencyCode===currency, `Currency should have been equal to "${expectedCurrencyCode}", but it is "${currency}."`);
-                    if (from===to) {
-                        assert(amount===convertedAmount,'The converted amount should have been equal to the initial one as the from and to currency symbols are the same.');
-                    }
-                })
-                .catch(err => {
-                    assert(false,err.message);
-                }) 
+            .then(convertedData => {
+                expect(convertedData).to.have.all.keys('convertedAmount', 'currency');
+                const { convertedAmount, currency } = convertedData;
+                expect(convertedAmount).to.be.a('number', 'Converted amount should have been a number, but it is a ', typeof (convertedAmount), '.');
+                assert(expectedCurrencySymbol === currency, `Currency should have been equal to "${expectedCurrencySymbol}", but it is "${currency}."`);
+                if (from === to) {
+                    assert(amount === convertedAmount, 'The converted amount should have been equal to the initial one as the from and to currency symbols are the same.');
+                }
+            })
+            .catch(err => {
+                assert(false, err.message);
+            })
     });
-} 
+}
+
+
+
